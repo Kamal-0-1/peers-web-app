ff8987f045c1d440d660833ad4eb0822c48405c6
Kamal <110046714+Kamal-0-1@users.noreply.github.com>
2025-11-08 23:23:26 +0530

    Developed local end connection setup

--- client/src/Video.jsx
+++ client/src/Video.jsx
@@ -1,20 +1,59 @@
 import { useEffect, useRef } from "react"
 
 export function Video(){
-    const v1=useRef(null);
+    const localVid=useRef(null);
+    const remoteVid=useRef(null);
+    const createOffer=useRef(null);
+    const createAnswer=useRef(null);
+    const offerArea=useRef(null);
+    const answerArea=useRef(null);
     useEffect(()=>{
         (async()=>{
-            v1.current.srcObject=await navigator.mediaDevices.getUserMedia(
-                {video:{
-                    width: { min: 300 },
-                    height: { min: 300 },},audio:false
-                });
-        })()
+            const peerConnection=new RTCPeerConnection({
+                iceServers:[{urls:"stun:stun.l.google.com:19302"}]
+            })
+
+            const localStream=await navigator.mediaDevices.getUserMedia({video:true});
+            localVid.current.srcObject=localStream;
+            localStream.getTracks().forEach((track)=>peerConnection.addTrack(track,localStream));
+
+            const remoteStream=new MediaStream();
+            remoteVid.current.srcObject=remoteStream;
+            peerConnection.ontrack=async(e)=>{
+                e.streams[0].getTracks().forEach((track)=>remoteStream.addTrack(track));
+            }
+
+            const ice=[];
+            peerConnection.onicecandidate=async(e)=>{
+                if(e.candidate){
+                    ice.push(e.candidate);
+                    console.log(e);
+                }
+                
+            }
+
+            createOffer.current.onclick=async()=>{
+                const offer=await peerConnection.createOffer();
+                await peerConnection.setLocalDescription(offer);
+                offerArea.current.value=JSON.stringify(peerConnection.localDescription);
+            }
+
+
+        })();
     },[]);
     return(
-        <div className="w-full h-screen flex justify-center items-center gap-3">
-            <video ref={v1} autoPlay></video>
-            <video></video>
+        <div className="w-full h-screen flex justify-center items-center gap-3 p-4">
+            <div className="w-1/2 h-full">
+                <video ref={localVid} autoPlay></video>
+                <button ref={createOffer} className="p-1 m-5 bg-gray-400">Create Offer</button>
+                <textarea ref={offerArea} className="w-full h-65"></textarea>
+            </div>
+            <div className="w-1/2 h-full">
+                <video ref={remoteVid} autoPlay></video>
+                <button ref={createAnswer} className="p-1 m-5 bg-gray-400">Create Answer</button>
+                <textarea ref={answerArea} className="w-full h-65"></textarea>
+            </div>
+            
         </div>
     )   
 }
